1425cd4464fa37b27a4477c36798a073aaf2f011
Iyida Clement Ikechukwu <iyidaclem@gmail.com>
2022-09-03 17:50:57 +0100

    Landing page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogDetailsComponent } from './blog-details/blog-details.component';
+import { HomeComponent } from './home/home.component';
 import { Landing2Component } from './landing2/landing2.component';
 import { Mint2Component } from './mint2/mint2.component';
 
 const routes: Routes = [
-  {path:"",component:Mint2Component},
+  {path:"",component:HomeComponent},
+   {path:"support",component:Mint2Component},
 {path:"about",component:BlogDetailsComponent},
 {path:"more",component:Landing2Component}
 ];
